63d72fdbc2eb5ec0949df2b2d93377b2c6d27cc4
mcm612 <michael.manguart@gmail.com>
2018-10-25 16:57:50 -0400

    hey

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,37 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5bcf85022f96508178a610a2';
+
+if(!ObjectID.isValid(id)) {
+    console.log('ID not valid');
+}
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//     console.log('Todos',todos);
+// });
+
+// //the first one found will be the result
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//     console.log('Todo',todo);
+// });
+
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo){
+//     return console.log('ID not found');
+//   }
+//   console.log('Todo By Id',todo);
+// }).catch((e) => console.log(e));
+User.findById(id).then((user) => {
+  if(!user){
+    return console.log('Unable to find user');
+  }
+  console.log(JSON.stringify(user,undefined,2));
+}).catch((e) => console.log(e));
