4f44e78725bdf32341fa3b88eba25d841ea033ec
mcm612 <michael.manguart@gmail.com>
2018-12-04 12:36:38 -0500

    Add POST /users route

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,65 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('User',{
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
-  });
+        minlength: 1,
+        unique: true, //verifies that the email is the only one in the collection
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            require: true
+        },
+        token: {
+            type: String,
+            require: true
+        }
+    }]
+});
+//we can limit the data that comes back when we convert one of our instances
+//into a JSON value
+//we can override a method to update exactly how mongoose certain things.
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    //user.toObject();
+    // takes user variable and converts it into an object
+    // where only the properties of the document exist
+    var userObject = user.toObject();
 
-  module.exports = {User};
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  //we need an access and token value in order to create a new token inside the document
+  //two arguments - data we want to sign, such as id and secret value
+  var token = jwt.sign({_id: user._id.toHexString(), access: access},'abc123').toString();
+  
+  //now we need to update the user tokens array
+  //before
+  //user.tokens.push({access, token});
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+      return token;
+  });
+};
+var User = mongoose.model('User', UserSchema);
+module.exports = {User};
   
