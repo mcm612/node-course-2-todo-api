f16acdf7eb3f75259518dd53e76b7f071a0fe0a0
mcm612 <michael.manguart@gmail.com>
2018-12-05 14:35:50 -0500

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -60,6 +60,27 @@ UserSchema.methods.generateAuthToken = function () {
       return token;
   });
 };
+
+//model method
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    //return a promise that always rejects
+    // return new Promise((resolve, reject) => {
+    //     reject();
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
 var User = mongoose.model('User', UserSchema);
 module.exports = {User};
   
